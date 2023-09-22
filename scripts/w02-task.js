7aa33d49067f48fd85a640044e2de593d0115689
Anzehlat <wol22013@byui.edu>
2023-09-22 11:44:33 -0600

    Creating and adding tasks for week 2

---
+++ scripts/w02-task.js
@@ -0,0 +1,30 @@
+/* W02-Task - Profile Home Page */
+
+/* Step 1 - Setup type tasks - no code required */
+
+/* Step 2 - Variables */
+let fullName = "Anzehla Wolfley";
+let currentYear = "";
+let profilePicture = "images/ProfilePic.jpg";
+
+/* Step 3 - Element Variables */
+const nameElement = document.getElementById("name");
+const foodElement = document.getElementById("food");
+const yearElement = document.querySelector("#year");
+
+imageElement = "images/ProfilePic.jpg";
+
+/* Step 4 - Adding Content */
+nameElement.innerHTML = "<strong>${fullName}</strong>";
+yearElement.textContent = "{currentYear}";
+imageElement.setAttribute("src", profilePicture);
+profilePicture.setAttribute("alt", "Profile image of {profilePicture}");
+
+/* Step 5 - Array */
+Sweets = [Icecream, Cookies]
+Savory = [Pasta, Salad, Chicken, Rice]
+
+
+
+
+
