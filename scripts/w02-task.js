eba3fb9a9581c8cd71a8c9962aacb7e4468001c5
Anzehlat <wol22013@byui.edu>
2023-09-23 18:53:26 -0600

    Fixed the array section

--- scripts/w02-task.js
+++ scripts/w02-task.js
@@ -4,34 +4,29 @@
 
 /* Step 2 - Variables */
 let fullName = "Anzehla Wolfley";
-let currentYear = "";
+let currentYear = Date();
 let profilePicture = "images/ProfilePic.jpg";
 
 /* Step 3 - Element Variables */
 const nameElement = document.getElementById("name");
 const foodElement = document.getElementById("food");
 const yearElement = document.querySelector("#year");
-const imageElement = document.getElementById("images/ProfilePic.jpg");
+const imageElement = document.getElementById("ProfilePic");
 
 /* Step 4 - Adding Content */
 nameElement.innerHTML = "<strong>Anzehla Wolfley</strong>";
-yearElement.textContent = "2023";
+yearElement.textContent = currentYear;
 imageElement.setAttribute('src', profilePicture);
 const altText = `Profile image of ${fullName}`;
 imageElement.setAttribute('alt', altText);
 
-
-
 /* Step 5 - Array */
-let favFoods = [Icecream, Cookies, Pasta, Salad, Chicken, Rice];
-let favoriteFood = "Steak";
-favoriteFood.push(favFoods);
-
-// Append the updated content to the existing content in the "food" element with a line break
-foodElement.innerHTML += `<br>${favFoods}`;
-favFoods.shift(); // Removes the first element from the Sweets array
-// Append the modified Sweets array with a line break
-foodElement.innerHTML += `<br>My favorite foods (updated): ${favFoods.join(", ")}`;
-favFoods.pop(); // Removes the last element from the Sweets array
-// Append the final modified Sweets array with a line break
-foodElement.innerHTML += `<br>My favorite foods (updated): ${favFoods.join(", ")}`;
+const food = ["Ice cream", "Cookies", "Pasta", "Salad", "Chicken", "Rice"]
+foodElement.innerHTML = `${food.join('<br>')}`;
+const anotherFood = "Steak";
+food.push(anotherFood);
+foodElement.innerHTML += `<br>${anotherFood}`;
+food.shift();
+foodElement.innerHTML += `<br>${food.join('<br>')}`;
+food.pop();
+foodElement.innerHTML += `<br>${food.join('<br>')}`;
