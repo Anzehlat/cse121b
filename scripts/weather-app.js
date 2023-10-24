ac401f38f162d4e906c1801635473fb77e75e96d
Anzehlat <wol22013@byui.edu>
2023-10-23 19:16:36 -0600

    Added simple css styling to center

--- scripts/weather-app.js
+++ scripts/weather-app.js
@@ -1,30 +1,57 @@
-// Anzehla Wolfley //
-// CSE121B Final Project //
+// Anzehla Wolfley
+// CSE121B Final Project
+
+const locations = [
+    "Salt Lake City, US",
+    "Phoenix, US",
+    "San Francisco, US",
+    "Honolulu, US"
+];
 
 async function getWeather() {
-    const location = document.getElementById('locationInput').value;
-    const apiKey = '82f6f533e71a536b482f678f0cb10dc2';
-    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${apiKey}&units=metric`;
+    const locationSelect = document.getElementById('locationSelect');
+    const selectedLocation = locationSelect.value;
+    const apiKey = 'd124e6d8f35a3e9f3118d4d2f61b6fb5';
 
-    try {
-        const response = await fetch(apiUrl);
-        if (!response.ok) {
-            throw new Error('Network response was not ok');
+    if (locations.includes(selectedLocation)) {
+        const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${selectedLocation}&appid=${apiKey}&units=metric`;
+
+        try {
+            const response = await fetch(apiUrl);
+            if (!response.ok) {
+                throw new Error('Network response not ok');
+            }
+            const data = await response.json();
+            displayWeather(data);
+        } catch (error) {
+            console.error('Error fetching weather data:', error);
+            displayError("Weather data couldn't be fetched. Please try again.");
         }
-        const data = await response.json();
-        displayWeather(data);
-    } catch (error) {
-        console.error('Error fetching weather data:', error);
+    } else {
+        console.error('Invalid location selected');
+        displayError('Invalid location selected. Please choose from the provided options.');
     }
 }
 
 function displayWeather(data) {
     const weatherDisplay = document.getElementById('weatherDisplay');
+    const locationName = `${data.name}, ${data.sys.country}`;
+    const temperature = `${data.main.temp}°C`;
+    const conditions = data.weather[0].description;
+    const windSpeed = `${data.wind.speed} m/s`;
+    const humidity = `${data.main.humidity}%`;
     weatherDisplay.innerHTML = `
-        <h2>Weather for ${data.name}, ${data.sys.country}</h2>
-        <p>Temperature: ${data.main.temp}°C</p>
-        <p>Conditions: ${data.weather[0].description}</p>
-        <p>Wind Speed: ${data.wind.speed} m/s</p>
-        <p>Humidity: ${data.main.humidity}%</p>
+        <h2>Weather for ${locationName}</h2>
+        <p>Temperature: ${temperature}</p>
+        <p>Conditions: ${conditions}</p>
+        <p>Wind Speed: ${windSpeed}</p>
+        <p>Humidity: ${humidity}</p>
     `;
 }
+
+function displayError(errorMessage) {
+    const weatherDisplay = document.getElementById('weatherDisplay');
+    weatherDisplay.innerHTML = `<p class="error">${errorMessage}</p>`;
+}
+
+getWeather();
